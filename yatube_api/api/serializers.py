9c8f09475b32e5aab22f78201606bfed3f703997
Gubarik <ead3471@gmail.com>
2022-11-30 13:22:42 +0300

    flake8 issues fixed

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,26 +1,30 @@
 from posts.models import Post, Group, Comment
 from rest_framework import serializers
 
+
 class PostSerializer(serializers.ModelSerializer):
-    group = serializers.SlugRelatedField(required=False, queryset = Group.objects.all(), slug_field='slug')
+    group = serializers.SlugRelatedField(required=False,
+                                         queryset=Group.objects.all(),
+                                         slug_field='slug')
     image = serializers.ImageField(required=False)
-    author =serializers.StringRelatedField(read_only=True)
+    author = serializers.StringRelatedField(read_only=True)
     pub_date = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = Post
-        fields=('id', 'author', 'text', 'group', 'image', 'pub_date')
+        fields = ('id', 'author', 'text', 'group', 'image', 'pub_date')
+
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields =('id','title', 'slug', 'description')
+        fields = ('id', 'title', 'slug', 'description')
         read_only_fields = ('id',)
 
+
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only = True)
+    author = serializers.StringRelatedField(read_only=True)
+
     class Meta:
         model = Comment
         fields = '__all__'
-
-
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,26 +1,31 @@
 from posts.models import Post, Group, Comment
 from rest_framework import viewsets
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
-from django.core.exceptions import PermissionDenied
-from django.contrib.auth.models import AnonymousUser
 from rest_framework.permissions import BasePermission, IsAuthenticated
 
 
 class IsOwnerOrReadOnly(BasePermission):
     message = "Изменение чужого контента запрещено!"
-    def has_object_permission(self, request, view:viewsets.ModelViewSet, obj):
-        return obj.author == request.user
+
+    def has_object_permission(self,
+                              request,
+                              view: viewsets.ModelViewSet, model):
+        return model.author == request.user
+
 
 class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [IsOwnerOrReadOnly, IsAuthenticated]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
-    serializer_class =GroupSerializer
+    serializer_class = GroupSerializer
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
